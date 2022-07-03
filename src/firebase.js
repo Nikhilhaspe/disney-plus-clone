90087a9cfe467a056c4ba6cfb3895bbdcfb7f22a
Nikhilhaspe <nikhilhaspe5220@gmail.com>
2022-07-03 20:36:18 +0530

    setup firebase with google authentication, setup header with navabar component

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCL5K9iKESmznAVjLUarsfjXJP9OuctVvE",
+  authDomain: "disneyplus-clone-eb959.firebaseapp.com",
+  projectId: "disneyplus-clone-eb959",
+  storageBucket: "disneyplus-clone-eb959.appspot.com",
+  messagingSenderId: "1066280934752",
+  appId: "1:1066280934752:web:11257224535f707c8d5205",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const storage = getStorage(app);
+
+export { auth, googleProvider, storage };
+export default db;
